f1810dbb605d55c4162053fdf8853d247eb0d8f8
jaelynbg <143460604+jaelynbg@users.noreply.github.com>
2023-11-30 12:12:12 -0800

    Update Salary by Major.R

--- Jaelyn - R Studio Code/Salary by Major.R
+++ Jaelyn - R Studio Code/Salary by Major.R
@@ -1,8 +1,24 @@
+library(ggplot2)
+
 #barplot starting median salary no color 
 ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = `Starting Median Salary`)) +
-  +     geom_bar(stat = "identity")
+  geom_bar(stat = "identity")
 
 #barplot starting median salary no color vertical x axis 
-ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = `Starting Median Salary`)) +
+ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = `Starting Median Salary`)) + 
+  geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+#starting at 30,000
+#Create the plot
+ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = AdjustedSalary)) + 
+  geom_bar(stat = "identity") +
+  scale_y_continuous(breaks = seq(0, 46000, by = 2000), labels = seq(30000, 76000, by = 2000)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+##green code 
+# Create the plot with conditional coloring
+ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = AdjustedSalary, fill = SalaryRange)) + 
   +     geom_bar(stat = "identity") +
+  +     scale_fill_manual(values = c("Under 35k" = "#adebad", "35k-50k" = "#5cd65c", "50k-65k" = "#33cc33", "65k-70k" = "#248f24", "Over 70k" = "#145214")) +
+  +     scale_y_continuous(breaks = seq(0, 46000, by = 2000), labels = seq(30000, 76000, by = 2000)) +
   +     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
