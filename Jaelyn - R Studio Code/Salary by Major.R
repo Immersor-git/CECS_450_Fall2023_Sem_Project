df1fd8d6adfb44343d64e1c22dbf28d976b5a5c6
jaelynbg <143460604+jaelynbg@users.noreply.github.com>
2023-12-02 15:20:32 -0800

    Update Salary by Major.R

--- Jaelyn - R Studio Code/Salary by Major.R
+++ Jaelyn - R Studio Code/Salary by Major.R
@@ -33,3 +33,29 @@ ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = AdjustedSalary, fill
                                      +                                  "Over 70k" = "#0d4d0d")) + # Very dark green for "Over 70k"
   +     scale_y_continuous(breaks = seq(0, 46000, by = 2000), labels = seq(30000, 76000, by = 2000)) +
   +     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ 
+ # Create a new column for coloring condition
+ degreespb$Color <- ifelse(degreespb$`Starting Median Salary` <= 35000, "Below or Equal to 35k", "Above 35k")
+ 
+ # Create the plot with conditional coloring
+ ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = `Starting Median Salary`, fill = Color)) + 
+   geom_bar(stat = "identity") +
+   scale_fill_manual(values = c("Below or Equal to 35k" = "red", "Above 35k" = "grey")) +
+   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ 
+ # Reorder 'Undergraduate Major' based on the median of 'Starting Median Salary'
+ degreespb$`Undergraduate Major` <- reorder(degreespb$`Undergraduate Major`, degreespb$`Starting Median Salary`, FUN = median)
+ 
+ # Create the ggplot
+ ggplot(data = degreespb, aes(x = `Undergraduate Major`, y = `Starting Median Salary`)) +
+   geom_bar(stat = "identity", fill = "green") +
+   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ 
+ 
+ # Count the number of unique undergraduate majors
+ number_of_majors <- length(unique(degreespb$`Undergraduate Major`))
+ 
+ # Print the result
+ number_of_majors
+ 
+ 
